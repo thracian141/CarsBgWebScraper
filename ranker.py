33b30ec636bb04c83f7eb5f771c431f870af193f
Bojo <bobata666@gmail.com>
2024-09-09 11:29:32 +0300

    changed if with match case

--- ranker.py
+++ ranker.py
@@ -3,174 +3,183 @@ import re
 
 # Define the functions for each criterion
 def price_points(price):
-    if price == 0:
-        return 0
-    if price < 2000:
-        return 10  
-    elif 2000 <= price <= 3000:
-        return 10 + 10 * (price - 2000) / (3000 - 2000) 
-    elif 3000 < price <= 6000:
-        return 20 * (6000 - price) / (6000 - 3000)  
-    else:
-        return 0  
+    match price:
+        case _ if price == 0:
+            return 0
+        case _ if price < 2000:
+            return 10  
+        case _ if 2000 <= price <= 3000:
+            return 10 + 10 * (price - 2000) / (3000 - 2000) 
+        case _ if 3000 < price <= 6000:
+            return 20 * (6000 - price) / (6000 - 3000)  
+        case _:
+            return 0  
 
 def city_points(city):
-    if 'София' in city:
-        return 8
-    elif 'Пловдив' in city:
-        return 10
-    elif 'Варна' in city:
-        return 6
-    elif 'Враца' in city:
-        return 1
-    elif 'Бургас' in city:
-        return 7
-    elif 'Стара Загора' in city:
-        return 8
-    elif 'Монтана' in city:
-        return 0
-    elif 'Плевен' in city:
-        return 2
-    elif 'Пазарджик' in city:
-        return 9
-    elif 'Смолян' in city:
-        return 4
-    elif 'Кърджали' in city:
-        return 3
-    elif 'Русе' in city:
-        return 2
-    elif 'Сливен' in city:
-        return 4
-    elif 'Хасково' in city:
-        return 4
-    elif 'Ямбол' in city:
-        return 4
-    elif 'Велико Търново' in city:
-        return 3
-    elif 'Габрово' in city:
-        return 3
-    elif 'Добрич' in city:
-        return 1
-    elif 'Казанлък' in city:
-        return 5
-    elif 'Видин' in city:
-        return 0
-    elif 'Кюстендил' in city:
-        return 5
-    elif 'Благоевград' in city:
-        return 6
-    elif 'Асеновград' in city:
-        return 10
-    else:
-        return 0
+    match city:
+        case _ if 'София' in city:
+            return 8
+        case _ if 'Пловдив' in city:
+            return 10
+        case _ if 'Варна' in city:
+            return 6
+        case _ if 'Враца' in city:
+            return 1
+        case _ if 'Бургас' in city:
+            return 7
+        case _ if 'Стара Загора' in city:
+            return 8
+        case _ if 'Монтана' in city:
+            return 0
+        case _ if 'Плевен' in city:
+            return 2
+        case _ if 'Пазарджик' in city:
+            return 9
+        case _ if 'Смолян' in city:
+            return 4
+        case _ if 'Кърджали' in city:
+            return 3
+        case _ if 'Русе' in city:
+            return 2
+        case _ if 'Сливен' in city:
+            return 4
+        case _ if 'Хасково' in city:
+            return 4
+        case _ if 'Ямбол' in city:
+            return 4
+        case _ if 'Велико Търново' in city:
+            return 3
+        case _ if 'Габрово' in city:
+            return 3
+        case _ if 'Добрич' in city:
+            return 1
+        case _ if 'Казанлък' in city:
+            return 5
+        case _ if 'Видин' in city:
+            return 0
+        case _ if 'Кюстендил' in city:
+            return 5
+        case _ if 'Благоевград' in city:
+            return 6
+        case _ if 'Асеновград' in city:
+            return 10
+        case _:
+            return 0
     
 def volume_points(volume):
-    if volume < 1.2:
-        return 0
-    elif 1.2 <= volume <= 2.0:
-        return 10 * (volume - 1.2) / (2.0 - 1.2) 
-    elif 2.0 < volume <= 3.0:
-        return 10 * (3.0 - volume) / (3.0 - 2.0) 
-    else:
-        return 0
+    match volume:
+        case _ if volume < 1.2:
+            return 0
+        case _ if 1.2 <= volume <= 2.0:
+            return 10 * (volume - 1.2) / (2.0 - 1.2) 
+        case _ if 2.0 < volume <= 3.0:
+            return 10 * (3.0 - volume) / (3.0 - 2.0) 
+        case _:
+            return 0
     
 def hp_points(hp):
-    if hp < 85:
-        return 0 
-    elif 85 <= hp <= 135:
-        return 10 * (hp - 85) / (135 - 85) 
-    elif 135 < hp <= 300:
-        return 10 * (300 - hp) / (300 - 135)  
-    else:
-        return 0 
+    match hp:   
+        case _ if hp < 85:
+            return 0 
+        case _ if 85 <= hp <= 135:
+            return 10 * (hp - 85) / (135 - 85) 
+        case _ if 135 < hp <= 300:
+            return 10 * (300 - hp) / (300 - 135)  
+        case _:
+            return 0 
   
 def brand_points(brand):
-    if 'volkswagen' in brand:
-        return 8
-    elif 'vw' in brand:
-        return 8
-    elif 'opel' in brand:
-        return 5
-    elif 'renault' in brand:
-        return 5
-    elif 'skoda' in brand:
-        return 5
-    elif 'ford' in brand:
-        return 4
-    elif 'peugeot' in brand:
-        return 6
-    elif 'citroen' in brand:
-        return 5
-    elif 'fiat' in brand:
-        return 3
-    elif 'toyota' in brand:
-        return 10
-    elif 'mazda' in brand:
-        return 7
-    elif 'honda' in brand:
-        return 9
-    elif 'hyundai' in brand:
-        return 8
-    elif 'nissan' in brand:
-        return 6
-    elif 'kia' in brand:
-        return 5
-    elif 'chevrolet' in brand:
-        return 4
-    elif 'seat' in brand:
-        return 4
-    elif 'mercedes' in brand:
-        return 2
-    elif 'bmw' in brand:
-        return -10
-    elif 'volvo' in brand:
-        return 9
-    elif 'alfa romeo' in brand:
-        return 6
-    elif 'dacia' in brand:
-        return 1
-    elif 'suzuki' in brand:
-        return 5
-    elif 'jaguar' in brand:
-        return 0
-    elif 'lancia' in brand:
-        return 6
-    else:
-        return 0
+    match brand:
+        case _ if 'volkswagen' in brand:
+            return 8
+        case _ if 'vw' in brand:
+            return 8
+        case _ if 'opel' in brand:
+            return 5
+        case _ if 'renault' in brand:
+            return 5
+        case _ if 'skoda' in brand:
+            return 5
+        case _ if 'ford' in brand:
+            return 4
+        case _ if 'peugeot' in brand:
+            return 6
+        case _ if 'citroen' in brand:
+            return 5
+        case _ if 'fiat' in brand:
+            return 3
+        case _ if 'toyota' in brand:
+            return 10
+        case _ if 'mazda' in brand:
+            return 7
+        case _ if 'honda' in brand:
+            return 9
+        case _ if 'hyundai' in brand:
+            return 8
+        case _ if 'nissan' in brand:
+            return 6
+        case _ if 'kia' in brand:
+            return 5
+        case _ if 'chevrolet' in brand:
+            return 4
+        case _ if 'seat' in brand:
+            return 4
+        case _ if 'mercedes' in brand:
+            return 2
+        case _ if 'bmw' in brand:
+            return -10
+        case _ if 'volvo' in brand:
+            return 9
+        case _ if 'alfa romeo' in brand:
+            return 6
+        case _ if 'dacia' in brand:
+            return 1
+        case _ if 'suzuki' in brand:
+            return 5
+        case _ if 'jaguar' in brand:
+            return 0
+        case _ if 'lancia' in brand:
+            return 6
+        case _:
+            return 0
 
-def mileage_points(mileage):
-    if mileage < 30000:
-        return 0
-    elif mileage == 'N/A':
-        return 0
-    elif mileage == '':
-        return 0
-    elif mileage > 320000:
-        return 0
-    elif mileage < 100000:
-        return 10
-    else:
-        return 10 * (320000 - mileage) / (320000 - 100000)
+
+def mileage_points(mileage):    
+    match mileage:
+        case _ if mileage < 30000:
+            return 0
+        case _ if mileage == 'N/A':
+            return 0
+        case _ if mileage == '':
+            return 0
+        case _ if mileage > 320000:
+            return 0
+        case _ if mileage < 100000:
+            return 10
+        case _:
+            return 10 * (320000 - mileage) / (320000 - 100000)
 
 def year_of_manufacture_points(year):
-    if year <= 2000:
-        return 0
-    elif year > 2010:
-        return 10
-    elif year > 2000 and year <= 2010:
-        return year - 2000
-    else:
-        return 0
+    match year:
+        case _ if year <= 2000:
+            return 0
+        case _ if year > 2010:
+            return 10
+        case _ if year > 2000 and year <= 2010:
+            return year - 2000
+        case _:
+            return 0
 
 def fuel_type_points(fuel_type):
-    if 'Дизел' in fuel_type:
-        return -5
-    elif 'Бензин' in fuel_type:
-        return 0
-    elif 'Газ/Бензин' in fuel_type:
-        return 5
-    else:
-        return 0
+    match fuel_type:
+        case _ if 'Дизел' in fuel_type:
+            return -5
+        case _ if 'Бензин' in fuel_type:
+            return 0
+        case _ if 'Газ/Бензин' in fuel_type:
+            return 5
+        case _:
+            return 0
 
 def calculate_points(car):
     points = 0
